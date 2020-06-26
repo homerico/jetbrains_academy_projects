cae6d9974b28f43a1a9ad6af1d9e78dd2224eca5
Eduardo de Moraes <homerico__main__@outlook.com>
2020-06-26 20:22:29 -0300

    Implement loop structure for users gameplay
    Stage 5/5: Fight!
    Objectives
    
    In this stage, you should write a program that:
     - Prints an empty field at the beginning of the game.
     - Creates a game loop where the program asks the user to enter the cell coordinates, analyzes the move for correctness and shows a field with the changes if everything is ok.
     - Ends the game when someone wins or there is a draw.
    You need to output the final result after the end of the game.

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -1,66 +1,75 @@
 # write your code here
-"""def moment_checker(tuple_):
+def moment_checker(tuple_):
     possibilities = [tuple_[0:3], tuple_[3:6], tuple_[6:9], tuple_[0::3],
                      tuple_[1::3], tuple_[2::3], tuple_[0::4], tuple_[2:7:2]]
-    match_x = tuple("XXX") in possibilities
-    match_o = tuple("OOO") in possibilities
-    return match_o, match_x"""
+    match_x = list("XXX") in possibilities
+    match_o = list("OOO") in possibilities
+    return match_o, match_x
+
+
+def checker_caller():
+    x_count = cells_list.count("X")
+    o_count = cells_list.count("O")
+    space_count = cells_list.count(" ")
+    checker = moment_checker(cells_list)
+
+    if abs(x_count - o_count) > 1 or checker == (True, True):
+        return states[4]
+    elif checker == (False, False) and space_count > 0:
+        return states[0]
+    elif checker == (False, False):
+        return states[1]
+    elif checker == (True, False):
+        return states[3]
+    else:
+        return states[2]
+
 
 def print_table():
-    global cells_tuple
-    print("""---------
+    global cells_list
+    print("""
+    ---------
     | %s %s %s |
     | %s %s %s |
     | %s %s %s |
-    ---------""" % tuple(cells_tuple))
+    ---------""" % tuple(cells_list))
+
 
 states = ["Game not finished", "Draw", "X wins", "O wins", "Impossible"]
 
-cells = input("Enter cells:")
-cells_tuple = list(cells)
+"""cells = "_________"
+cells_list = list(cells)"""
 standard_matrix = {}
 cells_index = 0
 
 for i in range(3):
     for j in range(3):
-      standard_matrix[(j + 1,3 - i)] = cells_tuple[cells_index]
-      cells_index += 1
-
+        standard_matrix[(j + 1, 3 - i)] = " " # cells_list[cells_index]
+        cells_index += 1
+cells_list = [standard_matrix[x] for x in standard_matrix]
 print_table()
 
+turn = 0
 # Trocar por try ... except
 while True:
+    user = ["X", "O"][turn % 2]
     coordinates = input("Enter the coordinates:").split()
     if len(coordinates[0]) == 1 and len(coordinates[1]) == 1:
         coordinates = [int(coordinate) for coordinate in coordinates]
         num_limit_checker = [1 <= checker <= 3 for checker in coordinates]
         if num_limit_checker == [True, True]:
-            if standard_matrix[tuple(coordinates)] is "_":
-                standard_matrix[tuple(coordinates)] = "X"
-                break
+            if standard_matrix[tuple(coordinates)] == " ":
+                standard_matrix[tuple(coordinates)] = user
+                turn += 1
+                cells_list = [standard_matrix[x] for x in standard_matrix]
+                print_table()
+                actual_state = checker_caller()
+                if actual_state != "Game not finished":
+                    print(actual_state)
+                    break
             else:
                 print("This cell is occupied! Choose another one!")
         else:
             print("Coordinates should be from 1 to 3!")
     else:
         print("You should enter numbers!")
-
-cells_tuple = [standard_matrix[x] for x in standard_matrix]
-print_table()
-
-"""x_count = cells_tuple.count("X")
-o_count = cells_tuple.count("O")
-space_count = cells_tuple.count("_")
-checker = moment_checker(cells_tuple)
-
-if abs(x_count - o_count) > 1 or checker == (True, True):
-    print(states[4])
-elif checker == (False, False) and space_count > 0:
-    print(states[0])
-elif checker == (False, False):
-    print(states[1])
-elif checker == (True, False):
-    print(states[3])
-else:
-    print(states[2])"""
-
