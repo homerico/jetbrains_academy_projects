de0cd701ce3141ab5d65533e1af397684cb55edd
Eduardo de Moraes <homerico__main__@outlook.com>
2020-06-26 20:12:08 -0300

    Check game state
    Stage 3/5: What's up on the field?
    Objectives
    
    In this stage, your program should:
    - Fill the field from the input and print it as in the previous stage.
    - Find the state in which the game is at the moment and print it. Possible states:
        "Game not finished" - when no side has a three in a row but the field has empty cells;
        "Draw" - when no side has a three in a row and the field has no empty cells;
        "X wins" - when the field has three X in a row;
        "O wins" - when the field has three O in a row;
        "Impossible" - when the field has three X in a row as well as three O in a row. Or the field has a lot more X's that O's or vice versa
    (if the difference is 2 or more, should be 1 or 0). For this stage, consider that the game can be started both as X's or as O's.
    Also, you can use ' ' or '_' to print empty cells - it's up to you.

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -1,4 +1,14 @@
 # write your code here
+def moment_checker(tuple_):
+    possibilities = [tuple_[0:3], tuple_[3:6], tuple_[6:9], tuple_[0::3],
+                     tuple_[1::3], tuple_[2::3], tuple_[0::4], tuple_[2:7:2]]
+    match_x = tuple("XXX") in possibilities
+    match_o = tuple("OOO") in possibilities
+    return match_o, match_x
+
+
+states = ["Game not finished", "Draw", "X wins", "O wins", "Impossible"]
+
 cells = input("Enter cells:")
 cells_tuple = tuple(cells)
 
@@ -7,3 +17,20 @@ print("""---------
 | %s %s %s |
 | %s %s %s |
 ---------""" % cells_tuple)
+
+x_count = cells_tuple.count("X")
+o_count = cells_tuple.count("O")
+space_count = cells_tuple.count("_")
+checker = moment_checker(cells_tuple)
+
+if abs(x_count - o_count) > 1 or checker == (True, True):
+    print(states[4])
+elif checker == (False, False) and space_count > 0:
+    print(states[0])
+elif checker == (False, False):
+    print(states[1])
+elif checker == (True, False):
+    print(states[3])
+else:
+    print(states[2])
+
