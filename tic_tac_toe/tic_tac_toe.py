d79c417ecddacd5070a75454d0a5c2ed2c581b99
Eduardo de Moraes <homerico__main__@outlook.com>
2020-06-26 20:15:23 -0300

    Implement and check basic coordinates input
    Stage 4/5: First move!
    Objectives:
    The program should work in the following way:
     - Get the 3x3 field from the input as in the previous stages.
     - Output this 3x3 field with cells before the user's move.
     - Then ask the user about his next move.
     - Then the user should input 2 numbers that represent the cell on which user wants to make his X or O. (9 symbols representing
       the field would be on the first line and these 2 numbers would be on the second line of the user input)
     - Analyze user input and show messages in the following situations:
    	"This cell is occupied! Choose another one!" - if the cell is not empty;
    	"You should enter numbers!" - if the user enters other symbols;
    	"Coordinates should be from 1 to 3!" - if the user goes beyond the field.
     - Then output the table including the user's most recent move.
    The program should also check user input. If the user input is unsuitable, the program should ask him to enter coordinates again.
    So, you need to output a field from the first line of the input and then ask the user to enter a move. Keep asking until the user
    enters coordinate that represents an empty cell on the field and after that output the field with that move. You should output the
    field only 2 times - before the move and after a legal move.

--- tic_tac_toe/tic_tac_toe.py
+++ tic_tac_toe/tic_tac_toe.py
@@ -1,24 +1,54 @@
 # write your code here
-def moment_checker(tuple_):
+"""def moment_checker(tuple_):
     possibilities = [tuple_[0:3], tuple_[3:6], tuple_[6:9], tuple_[0::3],
                      tuple_[1::3], tuple_[2::3], tuple_[0::4], tuple_[2:7:2]]
     match_x = tuple("XXX") in possibilities
     match_o = tuple("OOO") in possibilities
-    return match_o, match_x
+    return match_o, match_x"""
 
+def print_table():
+    global cells_tuple
+    print("""---------
+    | %s %s %s |
+    | %s %s %s |
+    | %s %s %s |
+    ---------""" % tuple(cells_tuple))
 
 states = ["Game not finished", "Draw", "X wins", "O wins", "Impossible"]
 
 cells = input("Enter cells:")
-cells_tuple = tuple(cells)
+cells_tuple = list(cells)
+standard_matrix = {}
+cells_index = 0
 
-print("""---------
-| %s %s %s |
-| %s %s %s |
-| %s %s %s |
----------""" % cells_tuple)
+for i in range(3):
+    for j in range(3):
+      standard_matrix[(j + 1,3 - i)] = cells_tuple[cells_index]
+      cells_index += 1
 
-x_count = cells_tuple.count("X")
+print_table()
+
+# Trocar por try ... except
+while True:
+    coordinates = input("Enter the coordinates:").split()
+    if len(coordinates[0]) == 1 and len(coordinates[1]) == 1:
+        coordinates = [int(coordinate) for coordinate in coordinates]
+        num_limit_checker = [1 <= checker <= 3 for checker in coordinates]
+        if num_limit_checker == [True, True]:
+            if standard_matrix[tuple(coordinates)] is "_":
+                standard_matrix[tuple(coordinates)] = "X"
+                break
+            else:
+                print("This cell is occupied! Choose another one!")
+        else:
+            print("Coordinates should be from 1 to 3!")
+    else:
+        print("You should enter numbers!")
+
+cells_tuple = [standard_matrix[x] for x in standard_matrix]
+print_table()
+
+"""x_count = cells_tuple.count("X")
 o_count = cells_tuple.count("O")
 space_count = cells_tuple.count("_")
 checker = moment_checker(cells_tuple)
@@ -32,5 +62,5 @@ elif checker == (False, False):
 elif checker == (True, False):
     print(states[3])
 else:
-    print(states[2])
+    print(states[2])"""
 
