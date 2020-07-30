0e307d77e5210b70d4a6c9c4f488b268dac5de24
Eduardo de Moraes <homerico__main__@outlook.com>
2020-07-30 15:44:07 -0300

    Add "Add income", "Do transfer" and "Close account" options
    Stage 4/4: Advanced system
    You have created the foundation of our banking system. Now let's take the opportunity to deposit money into an account, make transfers and close an account if necessary.
    Now your menu should look like this:
    1. Balance
    2. Add income
    3. Do transfer
    4. Close account
    5. Log out
    0. Exit
    If the user asks for Balance, you should read the balance of the account from the database and output it into the console.
    Add income item should allow us to deposit money to the account.
    Do transfer item should allow transferring money to another account. You should handle the following errors:
      > If the user tries to transfer more money than he/she has, output: "Not enough money!"
      > If the user tries to transfer money to the same account, output the following message: “You can't transfer money to the same account!”
      > If the receiver's card number doesn’t pass the Luhn algorithm, you should output: “Probably you made mistake in the card number. Please try again!”
      > If the receiver's card number doesn’t exist, you should output: “Such a card does not exist.”
      > If there is no error, ask the user how much money they want to transfer and make the transaction.
    If the user chooses the Close account item, you should delete that account from the database.
    Do not forget to commit your DB changes right after executing a query!

--- simple_banking_system/simple_banking_system.py
+++ simple_banking_system/simple_banking_system.py
@@ -1,14 +1,14 @@
 import sqlite3
 import random
-
-
+ 
+ 
 class Card:
     def __init__(self, card_num, pin):
         self.card_number = card_num
         self.card_pin = pin
         self.balace = 0
-
-
+ 
+ 
 def checksum(original_card_num):
     original_card_num = [int(num) for num in original_card_num]
     for index, num in enumerate(original_card_num):
@@ -17,11 +17,12 @@ def checksum(original_card_num):
         if original_card_num[index] > 9:
             original_card_num[index] -= 9
     return str((10 - (sum(original_card_num) % 10)) % 10)
-
-
+ 
+ 
 def create_account(customer_account_length=9):
     global id_
-    id_ += 1
+    while id_ in [intern_id[0] for intern_id in cur.execute("SELECT id FROM card")]:
+        id_ += 1
     card_num = "400000"
     pin = ''
     for i in range(customer_account_length):
@@ -38,21 +39,19 @@ def create_account(customer_account_length=9):
     );""", (id_, card_num, pin))
     conn.commit()
     return card_num, pin
-
-
+ 
+ 
 def log_in(user_card_num, user_pin):
     cur.execute("SELECT id FROM card WHERE number=? AND pin=?", (user_card_num, user_pin))
-    conn.commit()
-    user_permission = cur.fetchall()
-    print(user_permission)
-    if len(user_permission) >= 1:
+    user_permission = cur.fetchone()
+    if user_permission != None:
         print("You have successfully logged in!")
         return True, user_permission[0]
     else:
         print("Wrong card number or PIN!")
         return False, None
-
-
+ 
+ 
 credit_cards = list()
 id_ = 0
 execution = True
@@ -68,9 +67,9 @@ CREATE TABLE IF NOT EXISTS card (
 conn.commit()
 while execution:
     answer = int(input("""1. Create an account
-    2. Log into account
-    0. Exit
-    """))
+2. Log into account
+0. Exit
+"""))
     if answer == 0:
         break
     elif answer == 1:
@@ -87,19 +86,54 @@ Your card PIN:
         logged, card_id = log_in(user_card_num=user_card_num, user_pin=user_pin)
         while logged:
             logged_input = int(input("""1. Balance
-2. Log out
+2. Add income
+3. Do transfer
+4. Close account
+5. Log out
 0. Exit
 """))
             if logged_input == 0:
                 execution = False
                 break
             elif logged_input == 1:
-                cur.execute("""SELECT balance FROM card  where id=?""", card_id)
+                cur.execute("SELECT balance FROM card WHERE id=?", (card_id, ))
+                print("Balance: ",cur.fetchone()[0])
+            elif logged_input == 2:
+                income = int(input("Enter income:\n"))
+                cur.execute('UPDATE card SET balance=balance+? WHERE id=?', (income, card_id))
                 conn.commit()
-                print(cur.fetchall())
+                print("Income was added!\n")
+            elif logged_input == 3:
+                recipient = input("""Transfer\nEnter card number:\n""")
+                if recipient[-1] != checksum(recipient[0:-1]):
+                    print("Probably you made mistake in the card number. Please try again!")
+                    continue
+                cur.execute("SELECT id FROM card WHERE number=?", (recipient, ))
+                recipient_id = cur.fetchone()
+                if recipient_id is None:
+                    print("Such a card does not exist.")
+                    continue
+                elif recipient_id[0] == card_id:
+                    print("You can't transfer money to the same account!")
+                    continue
+                recipient_id = recipient_id[0]
+                value = int(input("Enter how much money you want to transfer:\n"))
+                cur.execute("SELECT balance FROM card WHERE id=?", (card_id, ))
+                if value > cur.fetchone()[0]:
+                    print("Not enough money!")
+                    continue
+                SQL_script = f"""UPDATE card SET balance=balance-{value} WHERE id={card_id};
+                                 UPDATE card SET balance=balance+{value} WHERE id={recipient_id}"""
+                cur.executescript(SQL_script)
+                conn.commit()
+                print("Success!")
+            elif logged_input == 4:
+                cur.execute("DELETE FROM card WHERE id=?", (card_id, ))
+                conn.commit()
+                print("The account has been closed!")
+                break
             else:
                 print("You have successfully logged out!")
                 break
 conn.close()
 print("Bye!")
-
