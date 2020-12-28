a1f5ed8102e3ef0a2845f9a739e9c09e95d925f7
Eduardo de Moraes <eduardommfloripa1@gmail.com>
2020-12-28 19:52:14 -0300

    Implement print and add tasks functionality
    Stage 2/4: I am an Alchemist!
    Let's store the data about our tasks in the database. Here's what you need to do:
    
      - Create a database file. Its name should be todo.db.
      - Create a table in this database. The table name should be task.
    
    The table task should have the following columns:
    
      - Integer column named id. It should be the primary key.
      - String column named task.
      - Date column named deadline. It should have the date when the task was created by default. You can use datetime.today() method.
    
    Also, you need to implement a menu that will make your program more convenient. The menu should have the following items:
    
     1. Today's tasks. Prints all tasks for today.
     2. Add task. Asks for task description and saves it in the database.
     3. Exit.

--- to_do_list/todolist.py
+++ to_do_list/todolist.py
@@ -1,5 +1,56 @@
-print("""Today:
-1) Do yoga
-2) Make breakfast
-3) Learn basics of SQL
-4) Learn what is ORM""")
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, Date, create_engine
+from datetime import datetime
+
+Base = declarative_base()
+
+
+class Table(Base):
+    __tablename__ = 'task'
+    id = Column(Integer, primary_key=True)
+    task = Column(String, default='Nothing')
+    deadline = Column(Date, default=datetime.today())
+
+    def __repr__(self):
+        return self.task
+
+
+def add_task(task):
+    global session
+    new_row = Table(task=task)
+    session.add(new_row)
+    session.commit()
+    print("The task has been added!")
+
+
+def show_all_tasks():
+    global session
+    rows = session.query(Table).all()
+    print("Today:")
+    if len(rows) == 0:
+        print("Nothing to do!")
+    else:
+        for i, row in enumerate(rows, start=1):
+            print(f"{i}. {row}")
+
+
+engine = create_engine('sqlite:///todo.db?check_same_thread=False')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+while True:
+    user_input = int(input("""1) Today's tasks
+2) Add task
+0) Exit\n"""))
+    if user_input == 0:
+        break
+    elif user_input == 1:
+        show_all_tasks()
+    elif user_input == 2:
+        user_input = input("Enter task\n")
+        add_task(user_input)
+
+print("Bye!")
+
