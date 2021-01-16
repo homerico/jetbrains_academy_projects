10166704c38f2cbf21c6be178eeabb59a8b3d52e
Eduardo de Moraes <eduardommfloripa1@gmail.com>
2021-01-15 22:06:43 -0300

    Add new functionalities on menu
    Add the following items to menu:
      - Week's tasks: prints all tasks for 7 days from today.
      - All tasks: prints all tasks sorted by deadline.
    Now Add task item should ask for the deadline of the task. Users should input the deadline in this format: YYYY-MM-DD.
    Also, Today's tasks item should print today's date.

--- to_do_list/todolist.py
+++ to_do_list/todolist.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, Integer, String, Date, create_engine
-from datetime import datetime
+from datetime import datetime, timedelta
 
 Base = declarative_base()
 
@@ -10,29 +10,54 @@ class Table(Base):
     __tablename__ = 'task'
     id = Column(Integer, primary_key=True)
     task = Column(String, default='Nothing')
-    deadline = Column(Date, default=datetime.today())
+    deadline = Column(Date, default=datetime.now())
 
     def __repr__(self):
         return self.task
 
 
-def add_task(task):
+def add_task(task, deadline):
     global session
-    new_row = Table(task=task)
+    new_row = Table(task=task, deadline=datetime.strptime(deadline, '%Y-%m-%d'))
     session.add(new_row)
     session.commit()
     print("The task has been added!")
 
 
-def show_all_tasks():
-    global session
-    rows = session.query(Table).all()
-    print("Today:")
+def print_this_day(rows):
     if len(rows) == 0:
         print("Nothing to do!")
     else:
         for i, row in enumerate(rows, start=1):
             print(f"{i}. {row}")
+
+
+def filter_by_date(date):
+    rows = session.query(Table).filter(Table.deadline == date.date())
+    rows = rows.order_by(Table.deadline).all()
+    return rows
+
+
+def show_all_tasks(period="all"):
+    global session
+    if period == "all":
+        rows = session.query(Table).order_by(Table.deadline).all()
+        print("All tasks:")
+        for i, row in enumerate(rows, start=1):
+            print(f"{i}. {row}. {row.deadline.strftime('%-d %b')}")
+    elif period == "today":
+        today = datetime.today()
+        rows = filter_by_date(today)
+        print(f"Today {today.strftime('%-d %b')}:")
+        print_this_day(rows)
+    elif period == "week":
+        today = datetime.today()
+        for i in range(7):
+            date = today + timedelta(days=i)
+            rows = filter_by_date(date)
+            print()
+            print(date.strftime("%A %-d %b:"))
+            print_this_day(rows)
 
 
 engine = create_engine('sqlite:///todo.db?check_same_thread=False')
@@ -42,15 +67,21 @@ session = Session()
 
 while True:
     user_input = int(input("""1) Today's tasks
-2) Add task
+2) Week's tasks
+3) All tasks
+4) Add task
 0) Exit\n"""))
     if user_input == 0:
         break
     elif user_input == 1:
-        show_all_tasks()
+        show_all_tasks("today")
     elif user_input == 2:
-        user_input = input("Enter task\n")
-        add_task(user_input)
+        show_all_tasks("week")
+    elif user_input == 3:
+        show_all_tasks()
+    elif user_input == 4:
+        task_outer = input("Enter task\n")
+        deadline_outer = input("Enter deadline\n")
+        add_task(task_outer, deadline_outer)
 
 print("Bye!")
-
